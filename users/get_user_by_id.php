e2a02fc438b10f59567654c8e76b6dc96072562c
Fanhao Yu <fy229@cornell.edu>
2025-06-09 12:03:45 -0400

    commit test

--- users/get_user_by_id.php
+++ users/get_user_by_id.php
@@ -13,6 +13,8 @@
     if ($conn->query($sql) !== FALSE) {
         $result = $conn->query($sql);
         echo "User retrieved successfully! <br>";
+        echo "User retrieved successfully! <br>";
+        echo "User retrieved successfully! <br>";
         while ($row = $result->fetch_assoc()) {
             echo "User ID: " . htmlspecialchars($row['id']) . "<br>";
             echo "Username: " . htmlspecialchars($row['name']) . "<br>";
