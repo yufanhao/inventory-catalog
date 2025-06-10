33bcec8b5332f2a91d4e94600dc1fa2d4a83a714
Fanhao Yu <fy229@cornell.edu>
2025-06-10 09:49:38 -0400

    Displaying boxes and cabinet in inventory

--- items/inventory.php
+++ items/inventory.php
@@ -6,7 +6,7 @@
         </h1>
         
         <h2>Add new item:</h2>
-        <form action="insert_item.php" method="POST">
+        <form action="insert_item.php" method="POST">i l
             Name: <input type="text" name="name"><br>
             Category: <input type="text" name="category"><br>
             Image: <input type="text" name="image_url"><br>
@@ -23,14 +23,23 @@
 
         
         echo "<table border='1' cellpadding='8'>";
-        echo "<tr><th>ID</th><th>Name</th><th>Category</th><th>Image</th><th>Expiration</th></tr>";
+        echo "<tr><th>ID</th><th>Name</th><th>Category</th><th>Image</th><th>Expiration</th><th>Box Number</th><th>Cabinet Number</th></tr>";
         while ($row = $result->fetch_assoc()) {
+            $item_id = $row['id'];
+            echo $item_id;
+            $box_id_sql = "SELECT box_id FROM item_box_join WHERE item_id = $item_id";
+            $box_id = $conn->query($box_id_sql);
+
+            $box_number_sql = "SELECT number FROM boxes WHERE id = $box_id";
+            $box_number = $conn->query($box_number_sql);
+
             echo "<tr>";
-            echo "<td><a href='get_item_by_id.php?id=" . $row['id'] . "'>" . htmlspecialchars($row['id']) .'</a></td>';
-            echo "<td><a href='get_item_by_id.php?id=" . $row['id'] . "'>" . htmlspecialchars($row['name']) .'</td>';
-            echo "<td>" . htmlspecialchars($row['category']) .'</a></td>';
-            echo "<td>" . htmlspecialchars($row['image_url']) .'</a></td>';
-            echo "<td>" . htmlspecialchars($row['expiration']) .'</a></td>';
+            echo "<td><a href='get_item_by_id.php?id=" . $row['id'] . "'>" . $row['id'] .'</a></td>';
+            echo "<td><a href='get_item_by_id.php?id=" . $row['id'] . "'>" . $row['name'] .'</td>';
+            echo "<td>" . $row['category'] .'</td>';
+            echo "<td>" . $row['image_url'] .'</td>';
+            echo "<td>" . $row['expiration'] .'</td>';
+            echo "<td>" . $box_number .'</td>';
             echo "<td><a href='delete_item.php?id=" . $row['id'] . "'>Delete Item</a></td>";
             echo "</tr>";
         }
