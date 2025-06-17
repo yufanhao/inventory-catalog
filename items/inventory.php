f53c0aa683856edae1ee5f46a3e83c3b81efa8db
Ahmed <ahmedhamiroune@gmail.com>
2025-06-17 11:23:23 -0400

    Fixed quantity funcitonality and worked on visual styles.

--- items/inventory.php
+++ items/inventory.php
@@ -1,6 +1,6 @@
 <head>
     <title>Item Catalog</title>
-    <link rel="stylesheet" href = "styles.css">
+    <link rel="stylesheet" href = "../styles.css">
 </head>
 
 <html>
@@ -14,7 +14,6 @@
             Image: <input type="file" id="fileInput" name="image_url"><br>
             Expiration: <input type="text" name="expiration"><br>
             Location: <input type="number" name="location"><br>
-            Quantity: <input type="number" name="quantity"><br>
             <input type="submit">
         </form>
         <p></p>
@@ -27,35 +26,54 @@
         <h2>Item list:</h2>
         <?php
         include '../db.php';
-        $result = $conn->query("SELECT * FROM items");
+        $result = $conn->query("SELECT * FROM items order by name");
 
-        
         echo "<table border='1' cellpadding='8'>";
-        echo "<tr><th>ID</th><th>Name</th><th>Category</th><th>Image</th><th>Expiration</th><th>Location</th><th>Quantity</th><th>Actions</th></tr>";
+        echo "<tr><th>Model Name</th><th>Category</th><th>Image</th><th>Expiration</th><th>Location</th><th>Quantity</th><th>Delete Item</th></tr>";
+        $myArray = array();
         while ($row = $result->fetch_assoc()) {
             $item_id = $row['id'];
+            $name = $row['name'];
+
+            //NOTE: Hack to remove duplicate items with same model name.
+            // real fix is to split the items table.
+            if (array_search($name, $myArray) !== false) {
+                continue;
+            }
+        
+            array_push($myArray, $name);
+
             $box_id_sql = "SELECT box_id FROM item_box_join WHERE item_id = $item_id";
+            
             $res = $conn->query($box_id_sql);
             $row2 = $res->fetch_assoc();
-            $box_id = $row2["box_id"];
-            $box_number_sql = "SELECT number FROM boxes WHERE id = $box_id";
-            $res = $conn->query($box_number_sql);
-            $row2 = $res->fetch_assoc();
-            $quantity_sql = "SELECT COUNT('id') AS Quantity FROM items GROUP by name";
+            $box_id = $row2['box_id'];
+
             
+            $box_number_sql = "SELECT number FROM boxes WHERE id = $box_id";
+            
+
+            $res2 = $conn->query($box_number_sql);
+            $row2 = $res2->fetch_assoc();
+
+            $quantity_id = $row['name'];
+            $quantity_sql = "SELECT distinct name, COUNT('id') AS quantity FROM items WHERE name = '$quantity_id' group by name" ;
+            $res3 = $conn->query($quantity_sql);
+            $row3 = $res3->fetch_assoc();
+            $quantity_id = $row3[1];
+
             echo "<tr>";
-            echo "<td><a href='get_item_by_id.php?id=" . $row['id'] . "'>" . $row['id'] ."</a></td>";
+            //echo "<td><a href='get_item_by_id.php?id=" . $row['id'] . "'>" . $row['id'] ."</a></td>";
             echo "<td><a href='get_item_by_id.php?id=" . $row['id'] . "'>" . $row['name'] ."</td>";
             echo "<td>" . $row['category'] ."</td>";
             echo '<td> <img src="' . $row["image_url"] .'"width="75" height="75" > </td>';
             echo "<td>" . $row['expiration'] ."</td>";
             echo "<td>" . $row2['number'] ."</td>";
-            echo "<td>" . $row['quantity'] ."</td>";
+            echo "<td>" . $row3['quantity'] ."</td>";
             echo "<td><a href='delete_item.php?id=" . $row['id'] . "'>Delete Item</a></td>";
             echo "</tr>";
         }
-
-        $conn->close()
-        ?>
-    </body>
+    $conn->close()
+?>
+</body>
 </html>
