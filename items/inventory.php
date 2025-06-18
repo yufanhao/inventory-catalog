b1865f39457bb7b574e8cf10aecba4a9fa5bdbb0
Fanhao Yu <fy229@cornell.edu>
2025-06-18 12:07:06 -0400

    Minor inventory display changes

--- items/inventory.php
+++ items/inventory.php
@@ -28,19 +28,20 @@
         <h2>Item list:</h2>
         <?php
         include '../db.php';
+
+        // Gets unique item names and their minimum ID for deletion
         $items = $conn->query("SELECT name, MIN(id) AS id FROM items GROUP BY name");
 
         echo "<table border='1' cellpadding='8'>";
-        echo "<tr><th>Name</th><th>Image</th><th>Quantity</th><th>Delete Item</th></tr>";
-        $myArray = array();
+        echo "<tr><th>Name</th><th>Image</th><th>Quantity</th></tr>";
         while ($row = $items->fetch_assoc()) {
+            // Counts the quantity of each item by name
             $count = $conn->query("SELECT COUNT(*) as quantity FROM items WHERE name = '" . $row['name'] . "'")->fetch_assoc();
 
             echo "<tr>";
             echo "<td><a href='get_item_by_name.php?name=" . $row['name'] . "'>" . $row['name'] ."</td>";
             echo '<td> <img src="' . $row["image_url"] .'"width="75" height="75" > </td>';
             echo "<td>" . $count['quantity'] ."</td>";
-            echo "<td><a href='delete_item.php?id=" . $row['id'] . "'>Delete Item</a></td>";
             echo "</tr>";
         }
         
