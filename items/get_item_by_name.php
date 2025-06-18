18321cb0e18744cfb792133504b75265da5440bb
Fanhao Yu <fy229@cornell.edu>
2025-06-18 12:04:59 -0400

    Removed duplicate display on inventory page, refactored displaying details of a specific item to another page when item is clicked

---
+++ items/get_item_by_name.php
@@ -0,0 +1,60 @@
+<html>
+    <body>
+    
+
+    <?php
+    require_once('../db.php');
+
+    $name = $_GET["name"];
+    echo"<h2>".$name."</h2>";
+
+    $items = $conn->query("SELECT * FROM items WHERE name = '$name'");
+    echo "<table border='1' cellpadding='8'>";
+    echo "<tr><th>Model Name</th><th>Category</th><th>Image</th><th>Expiration</th>
+          <th>Box</th><th>Cabinet</th><th>Shelf</th><th>Floor</th><th>Delete Item</th></tr>";
+    while ($row = $items->fetch_assoc()) {
+$location_id = $row['location_id'];
+        $location_sql = "SELECT * FROM locations WHERE id = $location_id";
+        $location = $conn->query($location_sql)->fetch_assoc();
+        $location_type = $location["type"];
+        $location_array = array(
+            'box' => "",
+            'cabinet' => "",
+            'shelf' => "",
+            'floor' => ""
+        );
+
+        // Traverse up the location hierarchy to get all location types and numbers
+        while ($location_type != 'ancestor') {
+            if (!$location) {
+                echo "Location not found for item: " . $row['name'] . ". Please check the database.";
+                break;
+            }
+
+            $location_number = $location['number'];
+            $location_array[$location_type] = $location_number;
+
+            $parent_id = $location['parent_id'];
+            $parent_sql = "SELECT * FROM locations WHERE id = $parent_id";
+            $parent = $conn->query($parent_sql)->fetch_assoc();
+            $location = $parent;
+            $location_type = $location["type"];
+        }
+
+        echo "<tr>";
+        echo "<td><a href='get_item_by_name.php?name=" . $row['name'] . "'>" . $row['name'] ."</td>";
+        echo "<td>" . $row['category'] ."</td>";
+        echo '<td> <img src="' . $row["image_url"] .'"width="75" height="75" > </td>';
+        echo "<td>" . $row['expiration'] ."</td>";
+        echo "<td>" . $location_array['box'] . "</td>";
+        echo "<td>". $location_array["cabinet"] ."</td>";
+        echo "<td>". $location_array["shelf"] ."</td>";
+        echo "<td>". $location_array["floor"] ."</td>";
+        echo "<td><a href='delete_item.php?id=" . $row['id'] . "'>Delete Item</a></td>";
+        echo "</tr>";
+    }
+
+    $conn->close();
+    ?>
+    </body>
+</html>
