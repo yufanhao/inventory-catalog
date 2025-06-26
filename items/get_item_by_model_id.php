215b6b122a702e82315bb1e1130e96a886a674d4
Ahmed <ahmedhamiroune@gmail.com>
2025-06-26 09:34:00 -0400

    Worked on search for items

--- items/get_item_by_model_id.php
+++ items/get_item_by_model_id.php
@@ -3,21 +3,21 @@
     <h2>Filter Items:</h2>
     <form method = "GET" action = "">
             Serial Number: <input type="text" name = "serial_number" placeholder = "Search items..." value =
-                "<?php echo isset($_GET['serial_number']) ? htmlspecialchars($_GET['serial_number']) : ''; ?>">
-            Expiration: <input type="date" name="expiration" disabled>
-            Before/After: <select name="before_after" disabled>
-                <option value="<=">Before</option>
-                <option value=">">After</option>
-            </select><br>
-            Location: <select name="location_type" disabled>
+                "<?php echo isset($_GET['serial_number']) ? htmlspecialchars($_GET['serial_number']) : ''; ?>"></br>
+            Before: <input type="date" name="before" value =
+                "<?php echo isset($_GET['before']) ? htmlspecialchars($_GET['before']) : ''; ?>">
+            After: <input type="date" name="after" value =
+                "<?php echo isset($_GET['after']) ? htmlspecialchars($_GET['after']) : ''; ?>"></br>
+            Location: <select name="location_type">
                 <option value="box">Box</option>
                 <option value="cabinet">Cabinet</option>
                 <option value="shelf">Shelf</option>
                 <option value="floor">Floor</option>
                 <option value="other">Other</option></select>
             Number(i.e. box number, etc): <input type="number" name = "number" placeholder = "Search items..." value =
-                "<?php echo isset($_GET['number']) ? htmlspecialchars($_GET['number']) : ''; ?>">
+                "<?php echo isset($_GET['number']) ? htmlspecialchars($_GET['number']) : ''; ?>"></br>
             <input type="hidden" name="searched" value="searched">
+            <input type="hidden" name="model_id" value="<?php echo isset($_GET['model_id']) ? htmlspecialchars($_GET['model_id']) : ''; ?>">
             <button type="submit">Search</button>
         </form>
 
@@ -28,88 +28,93 @@
     //$search = isset($_GET['search']) ? $conn->real_escape_string($_GET['search']) : '';
     $searched = isset($_GET['searched']) ? $conn->real_escape_string($_GET['searched']) : '';
     $serial_number = isset($_GET['serial_number']) ? $conn->real_escape_string($_GET['serial_number']) : '';
-    $expiration = isset($_GET['expiration']) ? $conn->real_escape_string($_GET['expiration']) : '';
-    $before_after = isset($_GET['before_after']) ? $conn->real_escape_string($_GET['before_after']) : '';
+    //$expiration = isset($_GET['expiration']) ? $conn->real_escape_string($_GET['expiration']) : '';
+    $before = isset($_GET['before']) ? $conn->real_escape_string($_GET['before']) : '';
+    $after = isset($_GET['after']) ? $conn->real_escape_string($_GET['after']) : '';
     $location_type = isset($_GET['location_type']) ? $conn->real_escape_string($_GET['location_type']) : '';
     $location_number = isset($_GET['number']) ? $conn->real_escape_string($_GET['number']) : '';
 
     $model_id = $_GET["model_id"];
-    
     $flag = FALSE;
-    
     $model_sql = "SELECT * FROM models WHERE id = '$model_id'";
     $model = $conn->query($model_sql)->fetch_assoc();
 
-    if ($searched !== "") 
-        //if($_GET['before_after'] === "<=") {
-            $items = $conn->query("SELECT * FROM items
-            WHERE serial_number like '%$serial_number%'
-            AND model_id = $model_id");
-          //  }
-        //else if ($_GET['before_after'] === ">") {
-          //  $items = $conn->query("SELECT * FROM items
-           // WHERE serial_number like '%$serial_number%'");
-            //AND expiration >= '$expiration'");
-     //   }
-    else
-        $items = $conn->query("SELECT * FROM items where model_id = $model_id" );
+    $selection = "SELECT * FROM items WHERE 1=1";
+        if ($model_id != '' AND $model != null) { // this should never happen, bc $model_id is a url parameter.
+        $selection = $selection . " AND model_id = $model_id";
+    }
+    
+    if ($searched !== "") {
+        if ($serial_number != '') { // this should never happen, bc $model_id is a url parameter.
+            $selection = $selection . " AND serial_number LIKE '%$serial_number%'";
+        }
 
-    // echo $items  . '<br>';
+        if ($before != '') { // this should never happen, bc $model_id is a url parameter.
+            $selection = $selection . " AND expiration <= Date('$before')";
+        }
+
+        if ($after != '') { // this should never happen, bc $model_id is a url parameter.
+            $selection = $selection . " AND expiration >= Date('$after')";
+        }
+    }  
+
+    echo $selection;
+    $items = $conn->query($selection); // this is the base query;
+    // at this point, $items has the final sql to execute include $model_id from url, and other values from filter form.
 
     echo"<h2>".$model['name']."</h2>"; // model_name
     echo "<table border='1' cellpadding='8'>";
     echo "<tr><th>Serial Number</th><th>Expiration</th>
           <th>Box</th><th>Cabinet</th><th>Shelf</th><th>Floor</th><th>Delete Item</th></tr>";
-    while ($row = $items->fetch_assoc()) {
-        $location_id = $row['location_id'];
-        $location_sql = "SELECT * FROM locations WHERE id = $location_id";
-        $location = $conn->query($location_sql)->fetch_assoc();
-        $location_type = $location["type"];
-        $flag = FALSE;
+    
+    if($items->num_rows > 0) 
+        while ($row = $items->fetch_assoc()) {
+            $location_id = $row['location_id'];
+            $location_sql = "SELECT * FROM locations WHERE id = $location_id";
+            $location = $conn->query($location_sql)->fetch_assoc();
+            $location_type = $location["type"];
+            $flag = FALSE;
 
-        $location_array = array(
-            'box' => "",
-            'cabinet' => "",
-            'shelf' => "",
-            'floor' => ""
-        );
+            $location_array = array(
+                'box' => "",
+                'cabinet' => "",
+                'shelf' => "",
+                'floor' => ""
+            );
 
-        // Traverse up the location hierarchy to get all location types and numbers
-        // TODO: Check post merge - while ($location_type != 'ancestor' AND $flag == FALSE){
-        while (($location_type !== 'ancestor') && ($flag === FALSE)) {
-           if (!$location) {
-                echo "Location not found for item: " . $row['serial_number'] . ". Please check the database.";
-                $flag = TRUE;
-                break;
+            // Traverse up the location hierarchy to get all location types and numbers
+            // TODO: Check post merge - while ($location_type != 'ancestor' AND $flag == FALSE){
+            while (($location_type !== 'ancestor') && ($flag === FALSE)) {
+            if (!$location) {
+                    //echo "Location not found for item: " . $row['serial_number'] . ". Please check the database.";
+                    $flag = TRUE;
+                    break;
+                }
+
+                $location_number = $location['number'];
+                $location_array[$location_type] = $location_number;
+                $parent_id = $location['parent_id'];
+                $parent_sql = "SELECT * FROM locations WHERE id = $parent_id";
+                $parent = $conn->query($parent_sql)->fetch_assoc();
+                
+                $location = $parent;
+                $location_type = $location["type"];            
             }
-
-            $location_number = $location['number'];
-            $location_array[$location_type] = $location_number;
-            $parent_id = $location['parent_id'];
-            $parent_sql = "SELECT * FROM locations WHERE id = $parent_id";
-            $parent = $conn->query($parent_sql)->fetch_assoc();
             
-            $location = $parent;
-            $location_type = $location["type"];            
-        }
-        
-        // echo $location_array['box'];
-        // echo "<br>";
-        echo $location_array['cabinet'];
-        echo "<br>";
-        echo $location_array['floor'];
-        echo "<br>";
 
-        echo "<tr>";
-        echo "<td><a href='get_item_by_id.php?id=" . $row['id'] . "'>" . $row['serial_number'] ."</td>";
-        echo "<td>" . $row['expiration'] ."</td>";
-        echo "<td>" . $location_array['box'] . "</td>";
-        echo "<td>". $location_array['cabinet'] ."</td>";
-        echo "<td>". $location_array['shelf'] ."</td>";
-        echo "<td>". $location_array['floor'] ."</td>";
-        echo "<td><a href='delete_item.php?id=" . $row['id'] . "'>Delete Item</a></td>";
-        echo "</tr>";
-    }
+            echo "<tr>";
+            echo "<td><a href='get_item_by_id.php?id=" . $row['id'] . "'>" . $row['serial_number'] ."</td>";
+            echo "<td>" . $row['expiration'] ."</td>";
+            echo "<td>" . $location_array['box'] . "</td>";
+            echo "<td>". $location_array['cabinet'] ."</td>";
+            echo "<td>". $location_array['shelf'] ."</td>";
+            echo "<td>". $location_array['floor'] ."</td>";
+            echo "<td><a href='delete_item.php?id=" . $row['id'] . "'>Delete Item</a></td>";
+            echo "</tr>";
+        }
+    else
+        echo "No rows returned.";
+
     echo '</table>';
     echo "<br><form action ='../models/view_models.php' method = 'get'>
                 <button type = 'submit'>Return to Inventory</button><br><br>
---
+++ models/upload_file.php
@@ -0,0 +1,76 @@
+<?php
+// DOC:
+// https://www.php.net/manual/en/features.file-upload.post-method.php
+
+
+// $src_file: Client file path, as stored in the _FILES_ server variable.
+// $target_dir: Server destination folder. - full path.
+// $file_type: type of files being uploaded. Default is image files.
+// $file_name: base file name. Set to basename of files location, if empty
+
+function upload_file($src_file , $target_dir, $file_type = 'image'){ 
+
+  // initialize expected values based on file types.
+  // for now, support images only. can expand in future.
+  $expected_extensions = array();
+  if ($file_type === 'image') {
+    $expected_extensions = array("jpg" , "png" , "jpeg","gif");
+  }
+
+  // get actual file attributes.
+    $uploadOk = 1;
+    $file_name = basename($_FILES[$src_file]["name"]); 
+    $target_file = $target_dir . '/' . $file_name;
+    $FileExt = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));    //echo 'in upload_file:' . $_FILES[$src_file]['name']. '<br>';
+    //echo 'target dir: ' . $target_dir . '<br>';
+    //echo 'target file: ' . $target_file . '<br>';
+    
+// Check file size
+if ($_FILES[$src_file]["size"] > 500000) {
+  echo "Sorry, your file {$file_name} is too large.<br>";
+  $uploadOk = 0;
+}
+
+// Validate file extensions certain file formats
+if (!in_array($FileExt , $expected_extensions)) {
+  echo "Sorry, your file {$file_name} doesn't have expected extension.<br>";
+  $uploadOk = 0;
+}
+
+// For image files, check if image file is a actual image or fake image
+if ($file_type === 'image') {
+  $check = getimagesize($_FILES[$src_file]["tmp_name"]);
+  if($check === false) {
+    echo "Sorry, your file {$file_name} is not an image.<br>" ;
+    $uploadOk = 0;
+  }
+}
+  
+// Check if file already exists
+// TODO: if it exists, skip, and return success.
+//if (file_exists($target_file)) {
+//  echo "Sorry, file already exists.";
+// $uploadOk = 0;
+//22}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file {$file_name} was not uploaded.<br>";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES[$src_file]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars($file_name) . " has been uploaded successfully.<br>";
+    return htmlspecialchars("/inventory-catalog/images/" . $file_name) ; 
+    //todo: remove hardcoded appName, but need relative path. 
+  } else {
+    echo "Sorry, there was an error uploading your file {$file_name}.<br>";
+    return FALSE;
+  }
+}
+}
+
+
+
+
+  
+?>
